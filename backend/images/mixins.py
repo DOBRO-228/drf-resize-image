b1a384d681e6cfad7e0fa899a6f6660a3ad30770
Dobro-228 <matematikaigor@gmailcom>
2022-02-08 00:06:52 +0300

    refactor

--- backend/images/mixins.py
+++ backend/images/mixins.py
@@ -23,36 +23,31 @@ class ImageHandlerMixin(object):
         """
         with tempfile.NamedTemporaryFile() as tmp_file:
             if request_payload.get('file'):
-                downloaded_file = request_payload.get('file')
+                image_to_save = request_payload.get('file')
             else:
-                downloaded_file = self.download_from_url(request_payload.get('url'), tmp_file)
-            with PILImage.open(downloaded_file) as image:
-                width, height = image.size
-            tmp_file.name = downloaded_file.name
+                tmp_file = self.download_from_url(request_payload.get('url'), tmp_file)
+                image_to_save = tmp_file
             return self.create_new_image_instance(
-                tmp_file,
-                width=width,
-                height=height,
+                image_to_save,
                 url=request_payload.get('url'),
             )
 
-    def download_from_url(self, url, temporary_file):
+    def download_from_url(self, url, tmp_file):
         """
-        Download image from url.
+        Download image from `url` and write it into `tmp_file`.
 
         Args:
-            temporary_file(file): Temporary file, where to write a downloaded image.
+            tmp_file(file): Temporary file, where to write a downloaded image.
 
         Returns:
-            temporary_file(file): Temporary file containing an image.
+            tmp_file(file): Temporary file containing an image.
         """
         with requests.get(url, stream=True) as downloaded_file:
             for chunk in downloaded_file.iter_content(chunk_size=8192):  # Noqa: WPS432
-                temporary_file.write(chunk)
+                tmp_file.write(chunk)
         path = urlparse(url).path
-        filename = path.split('/').pop()
-        temporary_file.name = filename
-        return temporary_file
+        tmp_file.name = path.split('/').pop()
+        return tmp_file
 
     def resize_image(self, request_payload, parent_object):
         """
@@ -65,53 +60,45 @@ class ImageHandlerMixin(object):
         Returns:
             image_object(models.Image): New instance of Image object.
         """
+        if request_payload.get('width'):
+            width = request_payload.get('width')
+        else:
+            width = parent_object.width
+        if request_payload.get('height'):
+            height = request_payload.get('height')
+        else:
+            height = parent_object.height
         with tempfile.NamedTemporaryFile() as tmp_file:
             with PILImage.open(parent_object.picture.file) as image:
-                properties = self.define_new_properties(
-                    image, request_payload, parent_object.picture.name,
-                )
-                resized_image = image.resize(
-                    (properties.get('width'), properties.get('height')),
-                )
+                resized_image = image.resize((int(width), int(height)))
                 resized_image.save(tmp_file, image.format)
-            tmp_file.name = properties.get('name')
+            tmp_file.name = self.define_new_name(request_payload, parent_object.picture.name)
             return self.create_new_image_instance(
                 tmp_file,
-                width=properties.get('width'),
-                height=properties.get('height'),
                 parent_object=parent_object,
             )
 
-    def define_new_properties(self, pillow_object, request_payload, parent_name):
+    def define_new_name(self, request_payload, parent_name):
         """
         Define new properties for image which need to resize.
 
         Args:
-            pillow_object(object): Opened image via Pillow.
             request_payload(dict): New width and height of image.
             parent_name(str): Parent Image which need to resize.
 
         Returns:
-            properties(dict): Properties for image which need to resize.
+            resized_image_name(str): Name of resized image.
         """
         name, ext = os.path.splitext(parent_name)
         if request_payload.get('width'):
-            width = request_payload.get('width')
-            name = '{0}_{1}'.format(name, width)
+            name = '{0}_{1}'.format(name, request_payload.get('width'))
         else:
-            width = pillow_object.width
             name = '{0}_0'.format(name)
         if request_payload.get('height'):
-            height = request_payload.get('height')
-            name = '{0}_{1}'.format(name, height)
+            name = '{0}_{1}'.format(name, request_payload.get('height'))
         else:
-            height = pillow_object.height
             name = '{0}_0'.format(name)
-        return {
-            'width': int(width),
-            'height': int(height),
-            'name': '{0}{1}'.format(name, ext),
-        }
+        return '{0}{1}'.format(name, ext)
 
     def create_new_image_instance(self, temporary_file, **kwargs):
         """
@@ -121,8 +108,9 @@ class ImageHandlerMixin(object):
             temporary_file(file): Temporary file containing an image.
 
         Returns:
-            properties(dict): Properties for image which need to resize.
+            image_object(models.Image): New instance of Image object.
         """
+        parent_picture = kwargs.get('parent_object')
         image_file = InMemoryUploadedFile(
             temporary_file,
             None,
@@ -131,16 +119,10 @@ class ImageHandlerMixin(object):
             None,
             None,
         )
-        if kwargs.get('parent_object'):
-            url = kwargs.get('parent_object').url
+        if parent_picture:
+            url = parent_picture.url
         else:
             url = kwargs.get('url')
-        image = Image(
-            url=url,
-            picture=image_file,
-            width=kwargs.get('width'),
-            height=kwargs.get('height'),
-            parent_picture=kwargs.get('parent_object'),
-        )
+        image = Image(url=url, picture=image_file, parent_picture=parent_picture)
         image.save()
         return image
--- backend/images/models.py
+++ backend/images/models.py
@@ -1,11 +1,10 @@
 from django.db import models
+from PIL import Image as PILImage
 
 
 class Image(models.Model):
     url = models.TextField(blank=True, null=True)
     picture = models.ImageField(blank=False)
-    width = models.PositiveIntegerField(blank=False)
-    height = models.PositiveIntegerField(blank=False)
     parent_picture = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL)
 
     @property
@@ -17,6 +16,28 @@ class Image(models.Model):
             name(str): Instance name.
         """
         return self.picture.path.split('/').pop()
+
+    @property
+    def width(self):
+        """
+        Define the width of the instance based on the picture's width.
+
+        Returns:
+            width(int): Instance width.
+        """
+        with PILImage.open(self.picture.file) as image:
+            return image.width
+
+    @property
+    def height(self):
+        """
+        Define the height of the instance based on the picture's height.
+
+        Returns:
+            height(int): Instance height.
+        """
+        with PILImage.open(self.picture.file) as image:
+            return image.height
 
     def delete(self):
         """
